4f03f5e17a08c39b96c933e26786be5dbaed4bbd
gophvan1230 <vangoph@qq.com>
2025-09-08 15:03:25 +0800

    5.4.3-2
    serde-aux crate从环境变量中提取整数

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,6 @@
 //! src/configuration.rs
 use secrecy::{ExposeSecret, Secret};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -11,6 +12,7 @@ pub struct Settings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -18,6 +20,7 @@ pub struct DatabaseSettings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
