51d5325eaa2f9ed116f983b474b39cd41a025d6b
gophvan1230 <vangoph@qq.com>
2025-09-09 01:46:13 +0800

    6.12.2
    validator crate 验证email格式是否有效

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -3,4 +3,5 @@ mod subscriber_email;
 mod new_subscriber;
 
 pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
 pub use new_subscriber::NewSubscriber;
--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,4 +1,46 @@
 use unicode_segmentation::UnicodeSegmentation;
+use validator::validate_email;
 
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+    use claim::{assert_err, assert_ok};
+    use tracing::Subscriber;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = " ".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomin.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domin.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+}
