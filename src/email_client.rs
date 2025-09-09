9aad0caf766e2d98852d7ffcde0d15b2a9b5391d
gophvan1230 <vangoph@qq.com>
2025-09-10 02:05:04 +0800

    7.2.5.1
    7.2.5.1 字符串切片&str只是指向其他对象所拥有的内存缓存区的指针;在结构体中存储引用，需添加一个生命周期参数，确保引用不会比指向的内存缓冲区存在更长的时间；

--- src/email_client.rs
+++ src/email_client.rs
@@ -13,12 +13,12 @@ pub struct EmailClient {
 
 #[derive(serde::Serialize)]
 #[serde(rename_all = "PascalCase")]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 impl EmailClient {
@@ -45,13 +45,13 @@ impl EmailClient {
     ) -> Result<(), reqwest::Error> {
         let url = format!("{}/email",self.base_url);
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject: subject,
+            html_body: html_content,
+            text_body: text_content,
         };
-        let builder = self.http_client
+        let _builder = self.http_client
             .post(&url)
             .header(
                 "X-Postmark-Server-Token",
@@ -67,8 +67,7 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
-
-    use actix_web::body;
+    
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
