1f6070adc32c63df8e5b03c9dd00708a770fa8f9
gophvan1230 <vangoph@qq.com>
2025-09-10 05:07:20 +0800

    7.2.6.2
    7.2.6.2一个经验法则：每当进行I/O操作时，都要设置超时时间！如果服务器的响应时间超过超时时间，则应该失败并返回错误;

--- src/email_client.rs
+++ src/email_client.rs
@@ -29,7 +29,10 @@ impl EmailClient {
         authorization_token: Secret<String>,
     ) -> Self {
         Self { 
-            http_client: Client::new(), 
+            http_client: Client::builder()
+                .timeout(std::time::Duration::from_secs(10))
+                .build()
+                .unwrap(),
             base_url, 
             sender,
             authorization_token,
@@ -169,6 +172,35 @@ mod tests {
 
         Mock::given(any())
             .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        claim::assert_err!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_times_out_if_the_server_takes_too_long() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(
+            mock_server.uri(),
+            sender, 
+            Secret::new(Faker.fake())
+        );
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let response = ResponseTemplate::new(200)
+            .set_delay(std::time::Duration::from_secs(180));
+        Mock::given(any())
+            .respond_with(response)
             .expect(1)
             .mount(&mock_server)
             .await;
