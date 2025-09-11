5ba81bf7f4ae5485c057efb91f570d10f9fbe984
gophvan1230 <vangoph@qq.com>
2025-09-12 01:02:55 +0800

    7.7.5.3
    7.7.5.3 重构集成测试

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,5 +1,4 @@
 use crate::helper::spawn_app;
-use reqwest::Url;
 use wiremock::{Mock, ResponseTemplate};
 use wiremock::matchers::{path, method};
 
@@ -30,26 +29,11 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
-
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(&raw_confirmation_link).unwrap();
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-    //设置URL中的端口
-    confirmation_link.set_port(Some(app.port)).unwrap();
-
-    let response = reqwest::get(confirmation_link)
+    let confirmation_links = app.get_confirmation_links(email_request);
+   
+    let response = reqwest::get(confirmation_links.html)
         .await
         .unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
-
 }
