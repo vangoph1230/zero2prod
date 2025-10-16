2b976d5a5d276096691d9cbcac68636767210bc2
gophvan1230 <vangoph@qq.com>
2025-10-17 02:26:32 +0800

    11.9.1

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,4 +1,8 @@
+use std::time::Duration;
+
 use crate::helper::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestApp};
+use crate::newsletter;
+use uuid::Uuid;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -169,4 +173,32 @@ async fn newsletter_creation_is_idempotent() {
     );
 
     // Mock在Drop上验证我们是否再次发送了邮件简报
+}
+
+#[tokio::test]
+async fn concurrent_form_submission_is_handled_gracefully() {
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(2)))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string(),
+    });
+
+    let response1 = app.post_publish_newsletter(newsletter_request_body);
+    let response2 = app.post_publish_newsletter(newsletter_request_body);
+    let (response1, response2) = tokio::join!(response1, response2);
+
+    assert_eq!( response1.status(), response2.status());
+    assert_eq!(response1.text().await.unwrap(), response2.text().await.unwrap());
 }
